ea1fc20bf8f01ff363da1da255e3ac9389ab0f24
medjed02 <kirill.maljaev@gmail.com>
2020-04-29 17:21:18 +0300

    object "Manga" is added

---
+++ data/mangas.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Manga(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'mangas'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    author = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    painter = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    translate = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    cnt_of_likes = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    date_of_release = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    translators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
