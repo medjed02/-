fd8c6a9c4adb9fc7491839c4137f5bf46a4fd62d
medjed02 <kirill.maljaev@gmail.com>
2020-05-06 19:22:25 +0300

    Delete and put are added for genres and for mangas

--- data/genre_resource.py
+++ data/genre_resource.py
@@ -2,6 +2,7 @@ from flask_restful import abort, Resource
 from data import db_session
 from data.genres import Genre
 from flask import jsonify
+from data import parsers
 
 
 def abort_if_genre_not_found(genre_id):
@@ -28,6 +29,33 @@ class GenresResource(Resource):
                 "genre": genre.to_dict()
             }
         )
+
+    def delete(self, genre_id):
+        abort_if_genre_not_found(genre_id)
+        args = parsers.genre_parser.parse_args()
+        if args['apikey'] != "specialkey":
+            return jsonify({'Access is denied': 'message'})
+        session = db_session.create_session()
+        genre = session.query(Genre).get(genre_id)
+        session.delete(genre)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+    def put(self, genre_id):
+        abort_if_genre_not_found(genre_id)
+        args = parsers.genre_parser.parse_args()
+        if args['apikey'] != "specialkey":
+            return jsonify({'Access is denied': 'message'})
+        session = db_session.create_session()
+        genre = session.query(Genre).get(genre_id)
+        for key in args:
+            if key == "name_of_genre":
+                genre.name = args[key]
+            elif key == "description":
+                genre.author = args[key]
+
+        session.commit()
+        return jsonify({'success': 'OK'})
 
 
 class GenresListResource(Resource):
