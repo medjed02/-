933e3ca85f6389028af7e55ae40c625e563d7647
Multiverse Mind <sanya.loginov03@yandex.ru>
2020-05-07 01:20:13 +0300

    Добавил старницы добавления жанра, манги и главы

---
+++ data/add_genre_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+
+class AddGenreForm(FlaskForm):
+    name_of_genre = StringField('Название жанра', validators=[DataRequired()])
+    description = StringField('Описание', validators=[DataRequired()])
+    cover = FileField('Обложка',
+                      validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Прикреплять можно только изображения')])
+    submit = SubmitField('Добавить жанр')
---
+++ data/add_manga_form.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+
+class AddMangaForm(FlaskForm):
+    name = StringField('Название манги', validators=[DataRequired()])
+    author = StringField('Автор', validators=[DataRequired()])
+    painter = StringField('Художник', validators=[DataRequired()])
+    translate = StringField('Перевод', validators=[DataRequired()])
+    date_of_release = IntegerField('Дата релиза', validators=[DataRequired('Дата релиза должна быть числом')])
+    translators = StringField('Переводчики', validators=[DataRequired()])
+    description = StringField('Описание', validators=[DataRequired()])
+    genres = StringField('Жанры', validators=[DataRequired()])
+    cover = FileField('Обложка',
+                      validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Прикреплять можно только изображения')])
+    submit = SubmitField('Добавить мангу')
